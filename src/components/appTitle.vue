c383965bbe00317f9748d554a22d60f6201b18e2
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-17 15:26:54 +0200

    add file

---
+++ src/components/appTitle.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="card">
+        <h1>La mia prima app con Vite!</h1>
+        <h2>E' proprio così</h2>   
+    </div>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        margin: 0;
+    }
+</style>
