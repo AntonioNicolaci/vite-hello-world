548ce42ebc443956c727b114a93ccb3abfe5ef73
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-17 16:24:27 +0200

    add jumbotron

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,8 +3,16 @@
 
 <template>
     <div id="jumbo">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img src="/vite.svg" class="logo logoVite" alt="Vite logo" />
+        <img src="../assets/vue.svg" class="logo logoVue" alt="Vue logo" />
+    </div>
+    <div id="linkDiv">
+        <div id="vite">
+            <a href="https://vitejs.dev/">Vai a Vite</a> 
+        </div>
+        <div id="vue">
+            <a href="https://vuejs.org/">Vai a Vue</a> 
+        </div>
     </div>
 </template>
 
@@ -23,5 +31,33 @@
     }
     .logo {
         height: 30em;
+    }
+    #linkDiv {
+        position: absolute;
+        top: 50vh;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        font-size: 5rem;
+    }
+    #vite a:hover,
+    #vite a:visited {
+        color: black;
+    }
+    #vue a:hover,
+    #vue a:visited {
+        color: #fff;
+    }
+    #vite,
+    #vue {
+        padding: 15px;
+        border-radius: 0.75rem;
+        margin-bottom: .3em;
+    }
+    #vite {
+        background: linear-gradient(0.25turn, #54BAFF, #BD34FE);
+    }
+    #vue {
+        color: #fff;
+        background: linear-gradient(0.25turn, #35495E, #41B883)
     }
 </style>
