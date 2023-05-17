8fa94c48b034ff6a2f9e209f8a55b9f33a3bc8cd
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-17 15:49:42 +0200

    add style in file/ change name in .header

--- src/components/appTitle.vue
+++ src/components/appTitle.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="card">
+    <div class="header">
         <h1>La mia prima app con Vite!</h1>
         <h2>E' proprio così</h2>   
     </div>
@@ -14,5 +14,11 @@
 <style scoped>
     h1 {
         margin: 0;
+    }
+    .header{
+        background-color: #0077ff;
+        border-bottom: 5px solid blue;
+        position: static;
+        z-index: 1;
     }
 </style>
