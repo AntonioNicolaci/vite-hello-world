04ad94a3c79b5899d57105336b5ef232d9b7a05f
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-17 15:49:00 +0200

    add AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AppTitle from './components/appTitle.vue'
+import AppHero from './components/AppHero.vue';
 </script>
 
 <template>
   <AppTitle/>
+  <AppHero/>
 </template>
 
 <style scoped>
