0c5569755754a2ecb34836b7f1e3649b76bec6bc
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-17 15:50:17 +0200

    add img/ add style

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+    <div id="jumbo">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </div>
+</template>
+
+<style scoped>
+    #jumbo {
+        top: 0;
+        left: 0;
+        position: fixed;
+        z-index: -1;
+        width: 100%;
+        height: 100vh;
+        background-color: blanchedalmond;
+        padding-top: 25vh;
+        display: flex;
+        justify-content: space-between;
+    }
+    .logo {
+        height: 30em;
+    }
+</style>
